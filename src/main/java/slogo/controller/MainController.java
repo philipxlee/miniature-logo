a12f974eddf4ee44e0c3838d9623b3c765fa9e61
Philip Lee <philipleektx@gmail.com>
2024-02-20 15:18:44 -0500

    SLOGO-66: added history to record entered commands and an iterator for view to access the command history

--- src/main/java/slogo/controller/MainController.java
+++ src/main/java/slogo/controller/MainController.java
@@ -1,16 +1,21 @@
 package slogo.controller;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
 import slogo.model.api.TurtleModel;
 import slogo.model.commands.Command;
 import slogo.model.parser.CommandParser;
 import slogo.view.TurtleView;
-
 
 public class MainController {
 
   private final TurtleModel turtleModel;
   private final TurtleView turtleView;
   private final CommandParser parser;
+  private final Queue<String> commandHistory;
+  private Iterator<String> commandHistoryIterator;
 
   /**
    * Constructor for MainController Initializes the turtle model, turtle view, and command parser
@@ -20,6 +25,7 @@ public class MainController {
     this.turtleModel = new TurtleModel();
     this.turtleView = new TurtleView();
     this.parser = new CommandParser(turtleModel);
+    this.commandHistory = new LinkedList<>();
   }
 
   /**
@@ -38,7 +44,28 @@ public class MainController {
   public void executeCommand(String command) {
     Command cmd = parser.parseCommand(command);
     cmd.execute();
+    commandHistory.add(command);
   }
+
+  /**
+   * Initializes or resets the iterator over the command history. Call this method when you want to
+   * start retrieving commands from the beginning.
+   */
+  public void resetCommandHistoryIterator() {
+    this.commandHistoryIterator = new LinkedList<>(commandHistory).iterator();
+  }
+
+  /**
+   * Retrieves the next most recent command without altering the original command history.
+   *
+   * @return the next command in history if available, or null if there are no more commands.
+   */
+  public Optional<String> getMostRecentCommand() {
+    return (commandHistoryIterator != null && commandHistoryIterator.hasNext()) ?
+        Optional.of(commandHistoryIterator.next()) :
+        Optional.empty();
+  }
+
 
   private void setObserverComponents() {
     turtleModel.addObserver(turtleView);
