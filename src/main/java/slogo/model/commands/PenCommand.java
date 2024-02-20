ff8dee6159e3506bfa8a67efe771a91f514c46ff
Philip Lee <philipleektx@gmail.com>
2024-02-20 14:18:00 -0500

    SLOGO-13, SLOGO-14: added penup and pendown functionality in command

---
+++ src/main/java/slogo/model/commands/PenCommand.java
@@ -0,0 +1,26 @@
+package slogo.model.commands;
+
+import slogo.model.api.TurtleModel;
+
+public class PenCommand implements Command {
+
+  private final TurtleModel model;
+  private final boolean penDown;
+
+  /**
+   * Constructs a PenCommand.
+   *
+   * @param model the TurtleModel to affect
+   * @param penDown true to put the pen down, false to lift it up
+   */
+  public PenCommand(TurtleModel model, boolean penDown) {
+    this.model = model;
+    this.penDown = penDown;
+  }
+
+  @Override
+  public void execute() {
+//    TODO: Create penDown() and penUp() in model
+//    penDown ? model.penDown() : model.penUp();
+  }
+}
