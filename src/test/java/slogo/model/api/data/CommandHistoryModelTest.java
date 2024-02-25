c5d5ba9fd35e3a6d5692e073e42acf29d84084b4
Arnav Nayak <arnavnayak2003@gmail.com>
2024-02-25 17:00:53 -0500

    added tests for command history model

--- src/test/java/slogo/model/api/data/CommandHistoryModelTest.java
+++ src/test/java/slogo/model/api/data/CommandHistoryModelTest.java
@@ -1,5 +1,43 @@
 package slogo.model.api.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Iterator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CommandHistoryModelTest {
 
+  private static final double TOLERANCE = 0.001;
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void testHistoryBasic() {
+    String command = "fd 50";
+    CommandHistoryModel commandHistoryModel = new CommandHistoryModel();
+    commandHistoryModel.addCommand(command);
+    Iterator<String> commands = commandHistoryModel.iterator();
+    assertTrue(commands.hasNext());
+    assertEquals(commands.next(), command);
+  }
+
+  @Test
+  void testHistoryDetailed() {
+    String[] commandStrings = new String[]{"fd 50", "cs", "rt 90", "bk 100", "rt 180", "home"};
+    CommandHistoryModel commandHistoryModel = new CommandHistoryModel();
+    for (String command : commandStrings) {
+      commandHistoryModel.addCommand(command);
+    }
+    Iterator<String> commands = commandHistoryModel.iterator();
+    assertTrue(commands.hasNext());
+    int i = 0;
+    while (commands.hasNext()) {
+      assertEquals(commandStrings[i], commands.next());
+      i++;
+    }
+  }
 }
