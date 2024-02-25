283d1218706b23d6055e242423f51b9946fab832
Philip Lee <philipleektx@gmail.com>
2024-02-25 13:16:35 -0500

    SLOGO-40 - SLOGO-48]: Changed execute() to return a double for parser compatability

--- src/main/java/slogo/model/command/bool/UnaryBoolCommand.java
+++ src/main/java/slogo/model/command/bool/UnaryBoolCommand.java
@@ -7,20 +7,31 @@ public class UnaryBoolCommand implements Command {
   private final double operand;
   private final OperationType operationType;
 
+  /**
+   * Constructs a UnaryBoolCommand, which performs a unary boolean operation
+   *
+   * @param operand the operand
+   * @param operationType the type of operation to perform
+   */
   public UnaryBoolCommand(double operand, OperationType operationType) {
     this.operand = operand;
     this.operationType = operationType;
   }
 
+  /**
+   * Executes the unary boolean command
+   *
+   * @return the result of the operation
+   */
   @Override
-  public void execute() { // change to double
-//    if (operationType == OperationType.NOT) {
-//      return operand == 0 ? 1.0 : 0.0;
-//    }
+  public Double execute() { // change to double
+    if (operationType == OperationType.NOT) {
+      return operand == 0 ? 1.0 : 0.0;
+    }
     throw new IllegalStateException("Unexpected operation type: " + operationType);
   }
 
-  public enum OperationType {
+  private enum OperationType {
     NOT
   }
 }
