79b43d67d04d766a17596a6a682e25441f24b642
Philip Lee <philipleektx@gmail.com>
2024-03-03 17:53:36 -0500

    [SLOGO-70, SLOGO-71]: Refactored colorToString into an abstract class to keep Color to Hex function DRY

--- src/main/java/slogo/view/buttons/actions/AbstractColorButton.java
+++ src/main/java/slogo/view/buttons/actions/AbstractColorButton.java
@@ -1,2 +1,33 @@
-package slogo.view.buttons.actions;public class AbstractColorButton {
+package slogo.view.buttons.actions;
+
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+/**
+ * AbstractColorButton provides the common functionality for buttons that use a ColorPicker
+ * to select and convert a color to a string.
+ */
+public abstract class AbstractColorButton extends ColorPicker {
+
+  /**
+   * Constructs a new AbstractColorButton with an initial color set.
+   *
+   * @param initialColor the initial color of the ColorPicker
+   */
+  public AbstractColorButton(Color initialColor) {
+    super(initialColor);
+  }
+
+  /**
+   * Converts a Color object to a hex string.
+   *
+   * @param color the Color to convert
+   * @return the hex string representation of the color
+   */
+  public String colorToString(Color color) {
+    return String.format("#%02X%02X%02X",
+        (int) (color.getRed() * 255),
+        (int) (color.getGreen() * 255),
+        (int) (color.getBlue() * 255));
+  }
 }
--- src/main/java/slogo/view/buttons/actions/PenColorButton.java
+++ src/main/java/slogo/view/buttons/actions/PenColorButton.java
@@ -1,38 +1,29 @@
 package slogo.view.buttons.actions;
 
-import javafx.scene.control.ColorPicker;
 import javafx.scene.paint.Color;
 import slogo.observer.PenColorObservable;
 
 /**
- * PenColorButton is a ColorPicker that updates a PenColorObservable with the selected color.
+ * PenColorButton is a ColorPicker that updates a PenColorObservable with the selected color,
+ * inheriting from AbstractColorButton.
  */
-public class PenColorButton extends ColorPicker {
+public class PenColorButton extends AbstractColorButton {
 
   private final PenColorObservable colorObservable;
 
   /**
-   * Constructs a new PenColorButton.
+   * Constructs a new PenColorButton with an initial color set.
+   *
+   * @param colorObservable the PenColorObservable to update
    */
   public PenColorButton(PenColorObservable colorObservable) {
-    super(Color.BLACK); // This sets the initial color of the button.
+    super(Color.BLACK);
     this.colorObservable = colorObservable;
     setOnAction(event -> updatePenColorObservable());
   }
 
-  /**
-   * Performs the action associated with the button.
-   */
-  public void updatePenColorObservable() {
+  private void updatePenColorObservable() {
     Color selectedColor = getValue();
     colorObservable.setColor(colorToString(selectedColor));
   }
-
-  private String colorToString(Color color) {
-    return String.format("#%02X%02X%02X",
-        (int) (color.getRed() * 255),
-        (int) (color.getGreen() * 255),
-        (int) (color.getBlue() * 255));
-  }
-
 }
