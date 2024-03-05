e5fa9f116d34bc486b8d89b6cc6ebc8ccf9cf3f2
Prince Ahmed <pa99@duke.edu>
2024-03-05 18:35:01 -0500

    slogo-61 updated user variables tab so that it now subscribes to the variables model and has an update function to display the info

--- src/main/java/slogo/view/tabs/UserVariablesTab.java
+++ src/main/java/slogo/view/tabs/UserVariablesTab.java
@@ -1,13 +1,24 @@
 package slogo.view.tabs;
 
+import java.util.Map;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+
 
 /**
  * UserVariablesTab is the tab that displays user defined variables.
  */
 public class UserVariablesTab implements TabContent {
+
+  private final VBox content;
+
+  /**
+   * Constructor for UserVariablesTab.
+   */
+  public UserVariablesTab() {
+    this.content = new VBox();
+  }
 
   /**
    * Return a node with the user variable content.
@@ -16,8 +27,19 @@ public class UserVariablesTab implements TabContent {
    */
   @Override
   public Node getContent() {
-    VBox content = new VBox();
-    content.getChildren().add(new Label("User Variables Tab Content"));
     return content;
+  }
+
+  /**
+   * Update the content of the tab with user-defined variables.
+   *
+   * @param variablesMap Map of variable names to their values
+   */
+  public void updateContent(Map<String, Double> variablesMap) {
+    content.getChildren().clear();
+    variablesMap.forEach((name, value) -> {
+      Label label = new Label(name + " = " + value);
+      content.getChildren().add(label);
+    });
   }
 }
